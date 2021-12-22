895165a11540e7f182786fc5c2dd95f3049f610f
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-22 16:15:13 +0200

    # Add the "FETCH_WEATHER_SUCCESS" and  "FETCH_WEATHER_FAIL" actions to the reducer

--- wether-app/src/reducers/weatherReducer.jsx
+++ wether-app/src/reducers/weatherReducer.jsx
@@ -4,12 +4,30 @@ export const primaryState = {
   countries: countriesNames,
   country: '',
   weatherData: {},
+  error: '',
 };
 
-const weatherReducer = (state = primaryState, { type }) => {
+const weatherReducer = (state = primaryState, { type, payload }) => {
   switch (type) {
     case 'RESET':
       return primaryState;
+    case 'FETCH_WEATHER_SUCCESS':
+      const fiveDaysWeather = [
+        payload.data[0],
+        payload.data[8],
+        payload.data[16],
+        payload.data[24],
+        payload.data[36],
+      ];
+      console.log('list', fiveDaysWeather);
+      return {
+        ...primaryState,
+        country: payload.country,
+        weatherData: fiveDaysWeather,
+      };
+    case 'FETCH_WEATHER_FAIL':
+      console.log('error', payload.error);
+      return { ...primaryState, error: payload.error };
     default:
       return state;
   }
