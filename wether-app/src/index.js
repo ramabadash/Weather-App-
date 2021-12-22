db38f797b5718a5842c2d74033ec0ef1d530cc1e
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-22 16:12:26 +0200

    # Add thunkMiddleware to the store

--- wether-app/src/index.js
+++ wether-app/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import weatherReducer from './reducers/weatherReducer';
+import thunkMiddleWare from 'redux-thunk';
 import App from './components/App';
 
-const store = createStore(weatherReducer);
+const store = createStore(weatherReducer, applyMiddleware(thunkMiddleWare));
 
 ReactDOM.render(
   <React.StrictMode>
