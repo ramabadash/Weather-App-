a5ed81c62cd2b1b1e3c5ac4231d2bd7498de8664
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-22 17:22:44 +0200

    # Add error handling popUp message

--- wether-app/src/components/App.jsx
+++ wether-app/src/components/App.jsx
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import CountriesDropDown from './CountriesDropDown';
 import WeatherList from './WeatherList';
+import { useSelector } from 'react-redux';
+// Pop Up messages
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  /***** STETS *****/
+  const errorMessage = useSelector(state => state.error);
+
+  /***** FUNCTIONS *****/
+  const notify = message =>
+    toast.error(`${message}`, {
+      position: 'top-right',
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+
+  /***** EFFECT *****/
+  useEffect(() => {
+    if (errorMessage) {
+      notify(errorMessage);
+    } else {
+      return;
+    }
+  }, [errorMessage]);
+
   return (
     <div>
+      <ToastContainer
+        position='top-right'
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <h1>What is the weather?</h1>
       <CountriesDropDown />
       <WeatherList />
