e700f9236b0a1665a7ffd5c9b341492fe7628969
ramabadash <89576646+ramabadash@users.noreply.github.com>
2021-12-22 16:14:29 +0200

    # Fetch weather by country on btn click by dispatch fetchWeater function

--- wether-app/src/components/CountriesDropDown.jsx
+++ wether-app/src/components/CountriesDropDown.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { reset } from '../actions/weatherActions';
+import axios from 'axios';
+import {
+  reset,
+  fetchWeatherSuccess,
+  fetchWeatherFail,
+} from '../actions/weatherActions';
 
 function CountriesDropDown() {
   /***** STATE *****/
@@ -8,12 +13,25 @@ function CountriesDropDown() {
   const countriesNames = useSelector(state => state.countries);
 
   /***** FUNCTIONS ******/
-  //
+  // Dispatch
   const dispatch = useDispatch();
-  //
-  const handleSubmit = () => {
-    console.log(country);
+
+  // Async Fetch weather request
+  const fetchWeather = () => {
+    return dispatch => {
+      axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/forecast?q=${country.toLocaleLowerCase()}&appid=d00f1c8526b61b691dc7eb25cf19e05c`
+        )
+        .then(({ data }) => {
+          dispatch(fetchWeatherSuccess(data.list, country));
+        })
+        .catch(error => {
+          dispatch(fetchWeatherFail(error.response.data.message));
+        });
+    };
   };
+
   // Clear country state and reset the state
   const handleClear = () => {
     setCountry('');
@@ -30,7 +48,13 @@ function CountriesDropDown() {
           setCountry(e.target.value);
         }}
       />
-      <button onClick={handleSubmit}>Submit</button>
+      <button
+        onClick={() => {
+          dispatch(fetchWeather());
+        }}
+      >
+        Submit
+      </button>
       <button onClick={handleClear}>Clear</button>
 
       <datalist id='countries'>
